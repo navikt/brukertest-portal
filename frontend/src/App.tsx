e3638d4ef5c93e581c43067080fdbc2e1bab581f
Thomas Rankin <thomas.rankin@nav.no>
2021-07-01 17:40:45 +0200

    lagt til NAV knapp som test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import './App.less';
-import Tekstomrade from 'nav-frontend-tekstomrade';
+import React from "react";
+import "./App.less";
+import Tekstomrade from "nav-frontend-tekstomrade";
+import { Knapp } from "nav-frontend-knapper";
 
 const App = (): JSX.Element => (
   <div className="App">
     <Tekstomrade>
-      {'Dette er et tekstområde. \n Dette er en ny linje.'}
+      {"Dette er et tekstområde. \n Dette er en ny linje."}
     </Tekstomrade>
-
+    <Knapp>NAV-knapp test</Knapp>
   </div>
 );
 
