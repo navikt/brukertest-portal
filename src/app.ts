7eeeb45cbb35eb11d82aaf23107f26bf5b811805
Emil Elton Nilsen <45180092+perkynades@users.noreply.github.com>
2021-07-02 14:10:14 +0200

    Legge inn routes for health checks

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('The sedulous hyena ate the antelope!');
 });
 
+app.get('/isalive', (req, res) => {
+    res.sendStatus(200)
+})
+
+app.get('/isready', (req, res) => {
+    res.sendStatus(200)
+})
+
 app.listen(port, () => {
     return console.log(`server is listening on ${port}`)
 })
