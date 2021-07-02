943492f025ddcb94d7807015fa8b501c2d820377
Emil Elton Nilsen <45180092+perkynades@users.noreply.github.com>
2021-07-02 16:32:49 +0200

    Legge til cors og annet serving

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,27 @@
 import express from 'express';
-import cors from 'cors'
 
 const app = express();
-const port = 3000;
+const port = 80;
 const router = express.Router()
 
-app.use(cors)
+const serverConfig = {
+    host: 'localhost',
+    port: 8080
+}
+
+const corsConfig = (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', serverConfig.host);
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, Content-Type, X-AUTHENTICATION, X-IP, Content-Type, Accept, Access-Control-Allow-Headers, Authorization, X-Requested-With'
+    );
+    res.setHeader('Access-Control-Expose-Headers', 'Location');
+    return next()
+}
+
+app.use(corsConfig)
 
 app.get('/isalive', (req, res) => {
     res.sendStatus(200)
@@ -14,7 +30,7 @@ app.get('/isalive', (req, res) => {
 app.get('/isready', (req, res) => {
     res.sendStatus(200)
 })
-
+router.use(express.static('public'))
 router.use('*', (req, res) => {
     res.sendFile('index.html', { root: 'public' })
 })
