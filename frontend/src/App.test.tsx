a520073c3cf1b28ea52b2a0b4e78e9dfd9c2c207
Emil Elton Nilsen <45180092+perkynades@users.noreply.github.com>
2021-07-05 12:41:12 +0200

    Fjerne auto-format på commit

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
+import React from "react"
+import { render, screen } from "@testing-library/react"
 
 it("renders learn react link", () => {
-  // render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-});
+    // render(<App />);
+    // const linkElement = screen.getByText(/learn react/i);
+    // expect(linkElement).toBeInTheDocument();
+})
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import "./App.less";
-import Tekstomrade from "nav-frontend-tekstomrade";
-import { Knapp } from "nav-frontend-knapper";
+import React from "react"
+import "./App.less"
+import Tekstomrade from "nav-frontend-tekstomrade"
+import { Knapp } from "nav-frontend-knapper"
 
 const App = (): JSX.Element => (
-  <div className="App">
-    <Tekstomrade>
-      {"Dette er et tekstområde. \n Dette er en ny linje."}
-    </Tekstomrade>
-    <Knapp>NAV-knapp test</Knapp>
-  </div>
-);
+    <div className="App">
+        <Tekstomrade>
+            {"Dette er et tekstområde. \n Dette er en ny linje."}
+        </Tekstomrade>
+        <Knapp>NAV-knapp test</Knapp>
+    </div>
+)
 
-export default App;
+export default App
