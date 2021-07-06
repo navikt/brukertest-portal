8fae737d62370623a839a7c2c8b2af394b096b12
Emil Elton Nilsen <45180092+perkynades@users.noreply.github.com>
2021-07-06 10:41:30 +0200

    Legge til fil for miljøvariabler

---
+++ backend/src/config/miljøvariabler.ts
@@ -0,0 +1,41 @@
+import dotenv from 'dotenv'
+import { cwd } from 'process'
+
+const miljøer = {
+    utvikling: 'development',
+    test: 'test',
+    produksjon: 'production'
+}
+const miljø = process.env.NODE_ENV ? process.env.NODE_ENV : miljøer.utvikling
+
+if (!Object.values(miljøer).find((val) => miljø === val)) {
+    throw new Error(
+        `${miljø} er ikke et gyldig kjøretidsmiljø\nVenligst sett NODE_ENV til en av disse: ${[
+            ...Object.values(miljøer)
+        ]}`
+    )
+}
+
+const src = miljø === miljøer.utvikling || miljø === miljøer.test ? 'src' : 'dist'
+
+if (miljø === miljøer.test) {
+    dotenv.config({ path: cwd() + '/.env.test'})
+} else {
+    dotenv.config()
+}
+
+export default {
+    database: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        db: process.env.POSTGRES_DB,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRESS_PASSWORD,
+        dropSchema: miljø === miljøer.test ? true : false
+    },
+    http: {
+        port: process.env.HTTP_PORT
+    },
+    miljø,
+    src
+}
