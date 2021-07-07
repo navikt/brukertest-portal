705ba42b0548eb28293087c3a0754ea49872e36a
Thomas Rankin <thomas.rankin@nav.no>
2021-07-07 10:47:45 +0200

    Laget footer

--- frontend/src/components/navigation/Footer.tsx
+++ frontend/src/components/navigation/Footer.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { ReactComponent as NavLogoSort } from "../../style/assets/Sort.svg";
 import Tekstomrade from "nav-frontend-tekstomrade";
+import "../../style/less/components/footer.less";
 
 export default function Footer() {
   return (
     <div className="footer">
       <NavLogoSort id="logo" />
-      <Tekstomrade>{"Innbyggerpanelet - en tjeneste fra NAV"}</Tekstomrade>
+      <Tekstomrade id="infotekst">
+        {"Innbyggerpanelet - en tjeneste fra NAV"}
+      </Tekstomrade>
     </div>
   );
 }
