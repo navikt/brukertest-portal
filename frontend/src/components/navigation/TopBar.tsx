768a0b0cd374be2fa3a444deed0d323a24828830
Thomas Rankin <thomas.rankin@nav.no>
2021-07-08 09:58:12 +0200

    #18 Lagt til login knapp på topbaren

--- frontend/src/components/navigation/TopBar.tsx
+++ frontend/src/components/navigation/TopBar.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Sidetittel } from "nav-frontend-typografi";
 import "../../style/less/components/topBar.less";
+import LoginButtonTopBar from "../../containers/button/LoginButtonTopBar";
 
 export default function TopBar() {
   return (
     <div className="topBar">
       <Sidetittel id="sidetittel">{"Innbyggerpanelet"}</Sidetittel>
+      <div id="loginBtn">
+        <LoginButtonTopBar />
+      </div>
     </div>
   );
 }
