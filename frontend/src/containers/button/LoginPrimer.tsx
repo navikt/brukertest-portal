c15aa4de7f695f9f8606d77319f4fd106ea0d868
Thomas Rankin <thomas.rankin@nav.no>
2021-07-08 14:16:46 +0200

    Redigert hovedknappen med oransje farger

--- frontend/src/containers/button/LoginPrimer.tsx
+++ frontend/src/containers/button/LoginPrimer.tsx
@@ -5,7 +5,7 @@ import { Hovedknapp } from "nav-frontend-knapper";
 export default function LoginPrimer() {
   return (
     <div className="loginPrimer">
-      <Hovedknapp>Logg inn</Hovedknapp>
+      <Hovedknapp id="hovedknapp">Logg inn</Hovedknapp>
     </div>
   );
 }
