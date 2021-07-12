536b822f4e02475202ffb7f03c0767bf16e1731a
Thomas Rankin <thomas.rankin@nav.no>
2021-07-12 19:02:31 +0200

    Fikset problemer med linting og prettier

--- frontend/src/components/navigation/Footer.tsx
+++ frontend/src/components/navigation/Footer.tsx
@@ -1,13 +1,13 @@
 import React from "react"
 import { ReactComponent as NavLogoSort } from "../../style/assets/Sort.svg"
 import Tekstomrade from "nav-frontend-tekstomrade"
-import "../../style/less/components/footer.less"
+import "../../style/less/components/navigation/footer.less"
 
 export default function Footer(): React.ReactElement {
     return (
         <div className="footer">
-            <NavLogoSort id="logo" />
-            <Tekstomrade id="infotekst">
+            <NavLogoSort className="logo" />
+            <Tekstomrade className="infotekst">
                 {"Innbyggerpanelet - en tjeneste fra NAV"}
             </Tekstomrade>
         </div>
--- frontend/src/components/navigation/TopBar.tsx
+++ frontend/src/components/navigation/TopBar.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { Sidetittel } from "nav-frontend-typografi";
-import "../../style/less/components/navigation/topBar.less";
-import LoginButtonTopBar from "../../containers/button/LoginButtonTopBar";
+import React from "react"
+import { Sidetittel } from "nav-frontend-typografi"
+import "../../style/less/components/navigation/topBar.less"
+import LoginButtonTopBar from "../../containers/button/LoginButtonTopBar"
 
-export default function TopBar() {
-  return (
-    <div className="topBar">
-      <Sidetittel className="sidetittel">{"Innbyggerpanelet"}</Sidetittel>
-      <div className="loginBtn">
-        <LoginButtonTopBar />
-      </div>
-    </div>
-  );
+export default function TopBar(): React.ReactElement {
+    return (
+        <div className="topBar">
+            <Sidetittel className="sidetittel">{"Innbyggerpanelet"}</Sidetittel>
+            <div className="loginBtn">
+                <LoginButtonTopBar />
+            </div>
+        </div>
+    )
 }
