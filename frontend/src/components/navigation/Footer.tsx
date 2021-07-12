c9c94f1694dbeef519495bcfd97a3ef8f3d1f06d
Emil Elton Nilsen <45180092+perkynades@users.noreply.github.com>
2021-07-12 12:16:35 +0200

    Fikse merge konflikt ting

--- frontend/src/components/navigation/Footer.tsx
+++ frontend/src/components/navigation/Footer.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { ReactComponent as NavLogoSort } from "../../style/assets/Sort.svg";
-import Tekstomrade from "nav-frontend-tekstomrade";
-import "../../style/less/components/footer.less";
+import React from "react"
+import { ReactComponent as NavLogoSort } from "../../style/assets/Sort.svg"
+import Tekstomrade from "nav-frontend-tekstomrade"
+import "../../style/less/components/Footer.less"
 
-export default function Footer() {
-  return (
-    <div className="footer">
-      <NavLogoSort id="logo" />
-      <Tekstomrade id="infotekst">
-        {"Innbyggerpanelet - en tjeneste fra NAV"}
-      </Tekstomrade>
-    </div>
-  );
+export default function Footer(): React.ReactElement {
+    return (
+        <div className="footer">
+            <NavLogoSort id="logo" />
+            <Tekstomrade id="infotekst">
+                {"Innbyggerpanelet - en tjeneste fra NAV"}
+            </Tekstomrade>
+        </div>
+    )
 }
--- frontend/src/views/landingsside/Landingsside.tsx
+++ frontend/src/views/landingsside/Landingsside.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { Knapp } from "nav-frontend-knapper";
-import "../../style/less/views/landingsside.less";
-import Footer from "../../components/navigation/Footer";
-import Tekstomrade from "nav-frontend-tekstomrade";
+import React from "react"
+import { Knapp } from "nav-frontend-knapper"
+import "../../style/less/views/landingsside.less"
+import Footer from "../../components/navigation/Footer"
+import Tekstomrade from "nav-frontend-tekstomrade"
 
-export default function Landingsside() {
-  return (
-    <div className="hovedOmrade">
-      <Tekstomrade>
-        {"Dette er et tekstområde. \n Dette er en ny linje."}
-      </Tekstomrade>
-      <Knapp>NAV-knapp test</Knapp>
-      <Footer />
-    </div>
-  );
+export default function Landingsside(): React.ReactElement {
+    return (
+        <div className="hovedOmrade">
+            <Tekstomrade>
+                {"Dette er et tekstområde. \n Dette er en ny linje."}
+            </Tekstomrade>
+            <Knapp>NAV-knapp test</Knapp>
+            <Footer />
+        </div>
+    )
 }
