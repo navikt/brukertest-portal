0b2e11cc2c4d14ea3407a3674fca78269d5fec93
Thomas Rankin <thomas.rankin@nav.no>
2021-07-13 09:59:43 +0200

    formatert filer riktig

--- frontend/src/components/hovedomrade/Hovedinnhold.tsx
+++ frontend/src/components/hovedomrade/Hovedinnhold.tsx
@@ -1,13 +1,13 @@
-import React from "react"
-import { Systemtittel } from "nav-frontend-typografi"
-import "../../style/less/components/hovedomrade/hovedinnhold.less"
-import LoginPrimer from "../../containers/button/LoginPrimer"
+import React from 'react'
+import { Systemtittel } from 'nav-frontend-typografi'
+import '../../style/less/components/hovedomrade/hovedinnhold.less'
+import LoginPrimer from '../../containers/button/LoginPrimer'
 
 export default function Hovedinnhold(): React.ReactElement {
     return (
         <div className="hovedinnhold">
             <Systemtittel className="logininfo">
-                {"Logg inn for å registrere ditt samtykke"}
+                {'Logg inn for å registrere ditt samtykke'}
             </Systemtittel>
             <LoginPrimer />
         </div>
--- frontend/src/components/hovedomrade/Hovedomrade.tsx
+++ frontend/src/components/hovedomrade/Hovedomrade.tsx
@@ -1,7 +1,7 @@
-import React from "react"
-import "../../style/less/components/hovedomrade/hovedomrade.less"
-import HovedomradeTopp from "./HovedomradeTopp"
-import Hovedinnhold from "./Hovedinnhold"
+import React from 'react'
+import '../../style/less/components/hovedomrade/hovedomrade.less'
+import HovedomradeTopp from './HovedomradeTopp'
+import Hovedinnhold from './Hovedinnhold'
 
 export default function Hovedomrade(): React.ReactElement {
     return (
