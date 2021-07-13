d935d9f95d702567701d41010061a4d166eae32f
Thomas Rankin <thomas.rankin@nav.no>
2021-07-13 10:55:09 +0200

    Lagt til App.test for at tester kan kjøre

---
+++ frontend/src/App.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+
+it('renders learn react link', () => {
+    // render(<App />);
+    // const linkElement = screen.getByText(/learn react/i);
+    // expect(linkElement).toBeInTheDocument();
+})
