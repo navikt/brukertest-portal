e3957d466ff63dcb3a6eff659d021fdda59048c6
Emil Elton Nilsen <45180092+perkynades@users.noreply.github.com>
2021-07-13 13:59:54 +0200

    Oppsett av test database ferdig

--- backend/tests/hjelpere/database.ts
+++ backend/tests/hjelpere/database.ts
@@ -1,12 +1,16 @@
 import { Connection, ConnectionOptions, createConnection } from 'typeorm'
 import miljøvariabler from '../../src/config/miljøvariabler'
+import dotenv from 'dotenv'
+import { cwd } from 'process'
 
 export const hentTestDatabase = async (): Promise<Connection> => {
+    dotenv.config({ path: cwd() + '/.env.test' })
+
     const testTypeormConfig: ConnectionOptions = {
         type: 'postgres',
         host: process.env.DB_HOST,
         port: Number.parseInt(process.env.DB_PORT!),
-        database: process.env.POSTGRES_DATABASE,
+        database: process.env.POSTGRES_DB,
         username: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
         synchronize: true,
