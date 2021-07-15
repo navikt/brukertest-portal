e6afd214e4198debb6f7678863a0c6a92f3a955b
Thomas Rankin <thomas.rankin@nav.no>
2021-07-15 15:14:49 +0200

    Fikset layout av topbar

--- frontend/src/components/navigation/TopBar.tsx
+++ frontend/src/components/navigation/TopBar.tsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Sidetittel } from 'nav-frontend-typografi'
 import '../../style/less/components/navigation/topBar.less'
 import LoginButtonTopBar from '../../containers/button/LoginButtonTopBar'
 import LogoutButtonTopBar from '../../containers/button/LogoutButtonTopBar'
 
 export default function TopBar(): React.ReactElement {
-    return (
-        <div className="topBar">
-            <LogoutButtonTopBar />
-            <Sidetittel className="sidetittel">{'Innbyggerpanelet'}</Sidetittel>
-            <LoginButtonTopBar />
-        </div>
-    )
+    const [loggedIn, setloggedIn] = useState<boolean>(true)
+
+    function renderItems() {
+        if (loggedIn) {
+            return (
+                <>
+                    <LogoutButtonTopBar />
+                    <Sidetittel className="sidetittel">
+                        {'Innbyggerpanelet'}
+                    </Sidetittel>
+                    <LoginButtonTopBar />
+                </>
+            )
+        } else {
+            return (
+                <Sidetittel className="sidetittel">
+                    {'Innbyggerpanelet'}
+                </Sidetittel>
+            )
+        }
+    }
+
+    return <div className="topBar">{renderItems()}</div>
 }
