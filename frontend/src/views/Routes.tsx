bddade829b0ee34a28006d21f7c3bb4d5fb18c46
Thomas Rankin <thomas.rankin@nav.no>
2021-07-15 09:05:52 +0200

    Fikset layout på topbar og footer

--- frontend/src/views/Routes.tsx
+++ frontend/src/views/Routes.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Landingsside from './landingsside/Landingsside'
-import Samtykkeskjema from './samtykkeskjema/Samtykkeskjema_00'
+import Samtykkeskjema from './samtykkeskjema/Samtykkeskjema'
 
 export default function Routes(): React.ReactElement {
     return (
