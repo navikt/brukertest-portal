2ddc494e4efad6ad619fc8d2ffbabc34738265d5
Emil Elton Nilsen <45180092+perkynades@users.noreply.github.com>
2021-07-22 09:31:10 +0200

    Legge inn "jeg forstår" steg

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import './style/less/main.less'
-//import Landingsside from './views/landingsside/Landingsside'
-import DeltakerUtfyllingSteg from './components/samtykkeskjema/DeltakerUtfyllingSteg'
+import JegForstårSteg from './components/samtykkeskjema/JegForstårSteg'
 
 const App = (): JSX.Element => (
     <div className="App">
-        <DeltakerUtfyllingSteg />
+        <JegForstårSteg />
     </div>
 )
 
