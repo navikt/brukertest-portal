c41eb88cf132126cca2a2d2620ab3cdf061ecdf4
Emil Elton Nilsen <45180092+perkynades@users.noreply.github.com>
2021-07-26 15:29:59 +0200

    Oppdatere håntering av errors på http endepunkt

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import samtykkeskjemaRuter from './ruter/samtykkeskjema'
 import administratorRuter from './ruter/administrator'
 import { hastighetsBegrensere } from './mellomvare/hastighetsBegrenser'
+import { errorBehandler } from './mellomvare/errorBehandler'
 
 const offentligeRuter = Router()
 offentligeRuter.use(hastighetsBegrensere.apiBegrenser)
@@ -10,4 +11,4 @@ offentligeRuter.use('/administrator', administratorRuter)
 
 const ruter = Router().use('/offentlig', offentligeRuter)
 
-export default Router().use('/api', ruter)
+export default Router().use('/api', ruter).use(errorBehandler)
--- backend/src/api/ruter/samtykkeskjema/index.ts
+++ backend/src/api/ruter/samtykkeskjema/index.ts
@@ -1,18 +1,13 @@
-import { DuplikatError } from '@/lib/errors/database/DuplikatError'
-import { IkkeFunnetError } from '@/lib/errors/database/IkkeFunnetError'
-import { FeilIEntitetError } from '@/lib/errors/validering/FeilIEntitetError'
 import { ISamtykkeskjema } from '@/modeller/Samtykkeskjema/ISamtykkeskjema'
 import { Samtykkeskjema } from '@/modeller/Samtykkeskjema/SamtykkeskjemaEntitet'
 import { Router } from 'express'
 import { StatusCodes } from 'http-status-codes'
-import { TomForespørselError } from 'lib/errors/rest/TomForespørselError'
 import { database } from '../../../lastere/laster'
-import { DårligForespørselError } from '../../../lib/errors/rest/DårligForespørselError'
 import { SamtykkeskjemaTjeneste } from '../../../tjenester/SamtykkeskjemaTjeneste'
 
 const ruter = Router()
 
-ruter.post('/', async (request, response) => {
+ruter.post('/', async (request, response, next) => {
     try {
         const nyttSamtykkeskjema = request.body as ISamtykkeskjema
         const samtykkeskjemaTjeneste = new SamtykkeskjemaTjeneste(database, nyttSamtykkeskjema.administrator)
@@ -23,39 +18,21 @@ ruter.post('/', async (request, response) => {
         response.status(StatusCodes.CREATED)
         response.send(samtykkeskjema)
     } catch (error) {
-        if (error instanceof DuplikatError) {
-            response.status(StatusCodes.BAD_REQUEST)
-            response.send('Dette samtykkeskjemaet finnes allerede')
-        } else if (error instanceof IkkeFunnetError) {
-            response.status(StatusCodes.BAD_REQUEST)
-            response.send('Ingen eier er tilkoblet dette samtykkeskjemaet')
-        } else if (error instanceof DårligForespørselError) {
-            response.status(StatusCodes.BAD_REQUEST)
-            response.send('Noe i forespørselen din er ikke riktig')
-        } else {
-            response.status(StatusCodes.INTERNAL_SERVER_ERROR)
-            response.send('Noe på serveren gikk gærnt...')
-        }
+        next(error)
     }
 })
 
-ruter.get('/', async (request, response) => {
+ruter.get('/', async (request, response, next) => {
     try {
         const samtykkeskjemaTjeneste = new SamtykkeskjemaTjeneste(database, request.body.administrator)
         const samtykkeskjemaer: Samtykkeskjema[] | undefined = await samtykkeskjemaTjeneste.hent()
         response.status(StatusCodes.OK).json(samtykkeskjemaer)
     } catch (error) {
-        if (error instanceof IkkeFunnetError) {
-            response.status(StatusCodes.NOT_FOUND)
-            response.send('Fant ingen samtykkeskjemaer')
-        } else {
-            response.status(StatusCodes.INTERNAL_SERVER_ERROR)
-            response.send('Noe på serveren gikk gærnt...')
-        }
+        next(error)
     }
 })
 
-ruter.get('/:id', async (request, response) => {
+ruter.get('/:id', async (request, response, next) => {
     try {
         const samtykkeskjemaTjeneste = new SamtykkeskjemaTjeneste(database, request.body.administrator)
         const id: number = Number.parseInt(request.params.id)
@@ -63,59 +40,31 @@ ruter.get('/:id', async (request, response) => {
         response.status(StatusCodes.OK)
         response.send(samtykkeskjema)
     } catch (error) {
-        if (error instanceof IkkeFunnetError) {
-            response.status(StatusCodes.NOT_FOUND)
-            response.send('Fant ikke samtykkeskjemaet')
-        } else {
-            response.status(StatusCodes.INTERNAL_SERVER_ERROR)
-            response.send('Noe på serveren gikk gærnt...')
-        }
+        next(error)
     }
 })
 
-ruter.put('/:id', async (request, response) => {
+ruter.put('/:id', async (request, response, next) => {
     try {
         const samtykkeskjemaTjeneste = new SamtykkeskjemaTjeneste(database, request.body.administrator)
         const id: number = Number.parseInt(request.params.id)
         const samtykkeskjema = request.body as ISamtykkeskjema
 
-        if (!samtykkeskjema) {
-            throw new TomForespørselError('Forespørselen er tom')
-        }
-
         const result = await samtykkeskjemaTjeneste.oppdater(id, samtykkeskjema)
         response.status(StatusCodes.OK).json(result)
     } catch (error) {
-        if (error instanceof FeilIEntitetError) {
-            response.status(StatusCodes.BAD_REQUEST)
-            response.send('Det var feil i det oppdaterte samtykkeskjemaet')
-        } else if (error instanceof TomForespørselError) {
-            response.status(StatusCodes.BAD_REQUEST)
-            response.send('Forespørselen er tom')
-        } else if (error instanceof IkkeFunnetError) {
-            response.status(StatusCodes.NOT_FOUND)
-            response.send('Fant ikke samtykkeskjemaet du prøvde å oppdatere')
-        } else {
-            response.status(StatusCodes.INTERNAL_SERVER_ERROR)
-            response.send('Noe på serveren gikk gærnt...')
-        }
+        next(error)
     }
 })
 
-ruter.delete('/:id', async (request, response) => {
+ruter.delete('/:id', async (request, response, next) => {
     try {
         const samtykkeskjemaTjeneste = new SamtykkeskjemaTjeneste(database, request.body.administrator)
         const id: number = Number.parseInt(request.params.id)
         const resultat = await samtykkeskjemaTjeneste.slett(id)
         response.status(StatusCodes.OK).json(resultat)
     } catch (error) {
-        if (error instanceof IkkeFunnetError) {
-            response.status(StatusCodes.NOT_FOUND)
-            response.send('Vi fant ikke samtykkeskjemaet som du prøver å slette')
-        } else {
-            response.status(StatusCodes.INTERNAL_SERVER_ERROR)
-            response.send('Noe på serveren gikk gærnt...')
-        }
+        next(error)
     }
 })
 
