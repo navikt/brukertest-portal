3c6057071091d31981713f21cb3a889d4d65721b
Thomas Rankin <thomas.rankin@nav.no>
2021-07-27 16:03:58 +0200

    Fikset en warning

--- frontend/src/containere/knapper/LoggUtKnappHovedinnhold.tsx
+++ frontend/src/containere/knapper/LoggUtKnappHovedinnhold.tsx
@@ -2,7 +2,7 @@ import { Flatknapp } from 'nav-frontend-knapper'
 import React from 'react'
 import { useAppStateDispatcher } from '../../kjerne/state/AppStateContext'
 
-export default function LoggUtKnappHovedinnhold() {
+export default function LoggUtKnappHovedinnhold(): React.ReactElement {
     const appDispatcher = useAppStateDispatcher()
 
     function oppdaterLoggInnState() {
