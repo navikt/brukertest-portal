8698bfe26177160870e04a1af3eb6cf622914c48
Emil Elton Nilsen <45180092+perkynades@users.noreply.github.com>
2021-07-28 14:39:45 +0200

    Legge til isAlive endepunkt

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -7,10 +7,12 @@ import { sjekkAdministratorAuth } from './mellomvare/autentisering'
 
 const offentligeRuter = Router()
 offentligeRuter.use(hastighetsBegrensere.apiBegrenser)
-offentligeRuter.use(sjekkAdministratorAuth)
+//offentligeRuter.use(sjekkAdministratorAuth)
 offentligeRuter.use('/samtykkeskjema', samtykkeskjemaRuter)
 offentligeRuter.use('/administrator', administratorRuter)
 
 const ruter = Router().use('/offentlig', offentligeRuter)
 
-export default Router().use('/api', ruter).use(errorBehandler)
+export default Router().use('/isAlive', (req, res) => {
+    res.send('Alive').status(200)
+}).use('/api', ruter).use(errorBehandler)
