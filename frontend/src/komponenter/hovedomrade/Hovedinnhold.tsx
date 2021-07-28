2c46777a80ae7fadf085a0bac46134c4b3e2f7ec
Thomas Rankin <thomas.rankin@nav.no>
2021-07-28 15:11:43 +0200

    Småfiks på forrige commit

--- frontend/src/komponenter/hovedomrade/Hovedinnhold.tsx
+++ frontend/src/komponenter/hovedomrade/Hovedinnhold.tsx
@@ -1,20 +1,6 @@
 import React from 'react'
-import { Systemtittel } from 'nav-frontend-typografi'
-import LoginPrimer from '../../containere/knapper/LoggInnHovedknapp'
-import { useAppState } from '../../kjerne/state/AppStateContext'
 import { HovedomradeProps } from './Hovedomrade'
 
 export default function Hovedinnhold(props: HovedomradeProps): React.ReactElement {
-    const { erLoggetInn } = useAppState()
-
-    return erLoggetInn ? (
-        <div className="hovedinnhold">{props.hovedInnhold}</div>
-    ) : (
-        <div className="hovedinnhold">
-            <Systemtittel className="logg-inn-info">
-                {'Logg inn for å registrere ditt samtykke'}
-            </Systemtittel>
-            <LoginPrimer />
-        </div>
-    )
+    return <div className="hovedinnhold">{props.hovedInnhold}</div>
 }
