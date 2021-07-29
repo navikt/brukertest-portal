c79787ec0ea60ad8282f9a7c3cbeee318110ba87
Emil Elton Nilsen <45180092+perkynades@users.noreply.github.com>
2021-07-29 15:28:20 +0200

    Riktig env navn nå?

--- backend/src/lastere/typeorm.ts
+++ backend/src/lastere/typeorm.ts
@@ -7,11 +7,11 @@ export default async () => {
 
     const typeormConfig: ConnectionOptions = {
         type: 'postgres',
-        host: process.env.NAIS_DATABASE_MYAPP_MYDB_HOST,
-        port: Number.parseInt(process.env.NAIS_DATABASE_MYAPP_MYDB_PORT!),
-        database: process.env.NAIS_DATABASE_MYAPP_MYDB_DATABASE,
-        username: process.env.NAIS_DATABASE_MYAPP_MYDB_USERNAME,
-        password: process.env.NAIS_DATABASE_MYAPP_MYDB_PASSWORD,
+        host: process.env.NAIS_DATABASE_BRUKERTEST_PORTAL_API_BRUKERTEST_PORTAL_DB_HOST,
+        port: Number.parseInt(process.env.NAIS_DATABASE_BRUKERTEST_PORTAL_API_BRUKERTEST_PORTAL_DB_PORT!),
+        database: process.env.NAIS_DATABASE_BRUKERTEST_PORTAL_API_BRUKERTEST_PORTAL_DB_DATABASE,
+        username: process.env.NAIS_DATABASE_BRUKERTEST_PORTAL_API_BRUKERTEST_PORTAL_DB_USERNAME,
+        password: process.env.NAIS_DATABASE_BRUKERTEST_PORTAL_API_BRUKERTEST_PORTAL_DB_PASSWORD,
         synchronize: true,
         logging: false,
     }
