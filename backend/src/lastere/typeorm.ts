9fac8a8f24b61618891158cf7c09eb029469aa49
Emil Elton Nilsen <45180092+perkynades@users.noreply.github.com>
2021-07-29 14:42:35 +0200

    Oppdatere env

--- backend/src/lastere/typeorm.ts
+++ backend/src/lastere/typeorm.ts
@@ -7,11 +7,11 @@ export default async () => {
 
     const typeormConfig: ConnectionOptions = {
         type: 'postgres',
-        host: process.env.DB_HOST,
-        port: Number.parseInt(process.env.DB_PORT!),
-        database: process.env.DB_DATABASE,
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
+        host: process.env.NAIS_DATABASE_MYAPP_MYDB_HOST,
+        port: Number.parseInt(process.env.NAIS_DATABASE_MYAPP_MYDB_PORT!),
+        database: process.env.NAIS_DATABASE_MYAPP_MYDB_DATABASE,
+        username: process.env.NAIS_DATABASE_MYAPP_MYDB_USERNAME,
+        password: process.env.NAIS_DATABASE_MYAPP_MYDB_PASSWORD,
         synchronize: true,
         logging: false,
     }
