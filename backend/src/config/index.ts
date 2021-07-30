a68c2bf98a714305d6d591ac13fab1d803577905
Emil Elton Nilsen <45180092+perkynades@users.noreply.github.com>
2021-07-30 13:56:09 +0200

    Commit før restart

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -3,10 +3,10 @@ import dotenv from 'dotenv'
 const miljø = process.env.NODE_ENV
 
 let src
-if (miljø === 'development') {
-    src = 'src'
-} else if (miljø === 'production') {
+if (miljø === 'production') {
     src = 'dist'
+} else {
+    src = 'src'
 }
 
 dotenv.config()
@@ -17,18 +17,18 @@ let port
 let db
 let user
 let password
-if (miljø === 'development') {
-    host = process.env.DB_HOST
-    port = process.env.DB_PORT
-    db = process.env.POSTGRES_DB
-    user = process.env.POSTGRES_USER
-    password = process.env.POSTGRES_PASSWORD
-} else if (miljø === 'production') {
+if (miljø === 'production') {
     host = process.env.NAIS_DATABASE_BRUKERTEST_PORTAL_API_BRUKERTEST_PORTAL_DB_HOST
     port = process.env.NAIS_DATABASE_BRUKERTEST_PORTAL_API_BRUKERTEST_PORTAL_DB_PORT
     db = process.env.NAIS_DATABASE_BRUKERTEST_PORTAL_API_BRUKERTEST_PORTAL_DB_DATABASE
     user = process.env.NAIS_DATABASE_BRUKERTEST_PORTAL_API_BRUKERTEST_PORTAL_DB_USERNAME
     password = process.env.NAIS_DATABASE_BRUKERTEST_PORTAL_API_BRUKERTEST_PORTAL_DB_PASSWORD
+} else {
+    host = process.env.DB_HOST
+    port = process.env.DB_PORT
+    db = process.env.POSTGRES_DB
+    user = process.env.POSTGRES_USER
+    password = process.env.POSTGRES_PASSWORD
 }
 
 export default {
--- backend/tests/hjelpere/database.ts
+++ backend/tests/hjelpere/database.ts
@@ -1,29 +1,6 @@
-import { Connection, ConnectionOptions, createConnection } from 'typeorm'
-import dotenv from 'dotenv'
-import { cwd } from 'process'
-import config from '@/config'
+import typeorm from '@/lastere/typeorm'
+import { Connection } from 'typeorm'
 
 export const hentTestDatabase = async (): Promise<Connection> => {
-    dotenv.config({ path: cwd() + '/.env.test' })
-
-    const testTypeormConfig: ConnectionOptions = {
-        type: 'postgres',
-        host: process.env.DB_HOST,
-        port: Number.parseInt(process.env.DB_PORT!),
-        database: process.env.POSTGRES_DB,
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        synchronize: true,
-        logging: false,
-        entities: [`${config.src}/modeller/**/*.{ts,js}`],
-        migrations: [`${config.src}/modeller/migration/**/*.{ts,js}`],
-        subscribers: [`${config.src}/modeller/subscriber/**/*.{ts,js}`],
-        cli: {
-            entitiesDir: `${config.src}/modeller`,
-            migrationsDir: `${config.src}/modeller/migration`,
-            subscribersDir: `${config.src}/modeller/subscriber`
-        }
-    }
-
-    return await createConnection(testTypeormConfig)
+    return await typeorm()
 }
