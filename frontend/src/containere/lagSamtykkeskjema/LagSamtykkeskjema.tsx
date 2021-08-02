9d2daab598d1984bd600ee65280d0398daf1991e
Emil Elton Nilsen <45180092+perkynades@users.noreply.github.com>
2021-08-02 12:25:44 +0200

    Legge til start dato og slutt dato

--- frontend/src/containere/lagSamtykkeskjema/LagSamtykkeskjema.tsx
+++ frontend/src/containere/lagSamtykkeskjema/LagSamtykkeskjema.tsx
@@ -1,11 +1,15 @@
+import { Datepicker } from 'nav-datovelger'
 import Hjelpetekst from 'nav-frontend-hjelpetekst'
 import { TextareaControlled } from 'nav-frontend-skjema'
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 
 export default function LagSamtykkeskjema(): ReactElement {
+    const [startDato, settStartDato] = useState<string>('')
+    const [sluttDato, settSluttDato] = useState<string>('')
+
     return (
         <div className="lag-samtykkeskjema">
-            <div className="invitasjon-overskrift">
+            <div className="sub-overskrift">
                 <h3>Invitasjon til deltagelse</h3>
                 <Hjelpetekst>Pass på å ikke avsløre deltakerens tilknyttning til NAV eller sensitive personopplysninger</Hjelpetekst>
             </div>
@@ -19,6 +23,29 @@ export default function LagSamtykkeskjema(): ReactElement {
                 maxLength={2000}
                 defaultValue=""
             />
+            <div className="sub-overskrift">
+                <h3>Hva skal undersøkelsen brukes til?</h3>
+                <Hjelpetekst>Datoen må være på format: DD.MM.ÅÅÅÅ</Hjelpetekst>
+            </div>
+            <TextareaControlled 
+                label="Undersøkelsen skal brukes til" 
+                maxLength={2000}
+                defaultValue=""
+            />
+            <div className="dato-container">
+                <p>Den gjennomføres</p>
+                <Datepicker 
+                    locale={'nb'}
+                    value={startDato}
+                    onChange={settStartDato}/>
+            </div>
+            <div className="dato-container">
+                <p>og varer til</p>
+                <Datepicker
+                    locale={'nb'}
+                    value={sluttDato}
+                    onChange={settSluttDato}/>
+            </div>
         </div>
     )
 }
