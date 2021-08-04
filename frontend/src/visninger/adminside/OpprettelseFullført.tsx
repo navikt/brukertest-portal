ad9dd6887dafe88a1fe615abcdadbe796575b9de
Thomas Rankin <thomas.rankin@nav.no>
2021-08-04 09:58:59 +0200

    Generer nå lenke med tilfeldig ID og kan kopieres ved knappetrykk

--- frontend/src/visninger/adminside/OpprettelseFullført.tsx
+++ frontend/src/visninger/adminside/OpprettelseFullført.tsx
@@ -6,12 +6,33 @@ import Hovedomrade from '../../komponenter/hovedomrade/Hovedomrade'
 import { Input } from 'nav-frontend-skjema'
 
 export default function OpprettelseFullført(): React.ReactElement {
+    const lenkeBase = 'www.innbyggerpanelet.no/'
+
+    // Lager en lenke med en tilfeldig ID på enden
+    const lenkeTekst = lenkeBase.concat(lagID(8))
+
+    // Lager en tilfeldig string
+    function lagID(lengde: number) {
+        let resultat = ''
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+        const charactersLength = characters.length
+        for (let i = 0; i < lengde; i++) {
+            resultat += characters.charAt(Math.floor(Math.random() * charactersLength))
+        }
+        return resultat
+    }
+
     const Innhold = () => (
         <>
             <h2 className="typo-undertittel">Send ut denne lenken til de du skal intervjue:</h2>
             <div className="lenke-komponent">
-                <Input value="lenke.no" className="lenke-input" />
-                <Hovedknapp className="lenke-kopi-knapp">
+                <Input value={lenkeTekst} className="lenke-input" />
+                <Hovedknapp
+                    className="lenke-kopi-knapp"
+                    onClick={() => {
+                        navigator.clipboard.writeText(lenkeTekst)
+                    }}
+                >
                     <Link />
                     <span> Kopier lenke</span>
                 </Hovedknapp>
