2dbb05d0447320d585622fb67911c7d021d3f667
Thomas Rankin <thomas.rankin@nav.no>
2021-08-05 11:17:37 +0200

    Gjort om meny-items til trykkbare lenker

--- frontend/src/visninger/adminside/AdminProfilside.tsx
+++ frontend/src/visninger/adminside/AdminProfilside.tsx
@@ -1,10 +1,13 @@
 import { AddCircle, Calender, FileContent } from '@navikt/ds-icons'
-import { Innholdstittel, Systemtittel } from 'nav-frontend-typografi'
+import { Systemtittel } from 'nav-frontend-typografi'
 import React from 'react'
 import Hovedomrade from '../../komponenter/hovedomrade/Hovedomrade'
 import { ReactComponent as ProfilIkon } from '../../style/ressurser/Profil-ikon.svg'
+import { LenkepanelBase } from 'nav-frontend-lenkepanel'
 
 export default function AdminProfilside(): React.ReactElement {
+    // Hvert hovedomrade blir wrappet inni et lenkepanel-komponent for å sikre universelt design
+
     return (
         <div className="admin-profilside">
             <div>
@@ -12,36 +15,42 @@ export default function AdminProfilside(): React.ReactElement {
                 <Systemtittel className="brukernavn">Admin Istrator</Systemtittel>
             </div>
             <div className="meny-items">
-                <Hovedomrade
-                    tittel={'Mine samtykkeskjemaer'}
-                    toppIkon={<FileContent />}
-                    hovedInnhold={
-                        <p className="typo-normal">
-                            Her kan du se, redigere eller slette de samtykkeskjemaene du har
-                            opprettet. Du finner også lenken du kan dele med brukeren.
-                        </p>
-                    }
-                />
-                <Hovedomrade
-                    tittel={'Opprett samtykkeskjema'}
-                    toppIkon={<AddCircle />}
-                    hovedInnhold={
-                        <p className="typo-normal">
-                            Her kan du opprette samtykkeskjema til brukertester, intervjuer og
-                            spørreundersøkelser som du kan dele med brukeren.
-                        </p>
-                    }
-                />
-                <Hovedomrade
-                    tittel={'Aktiviteter'}
-                    toppIkon={<Calender />}
-                    hovedInnhold={
-                        <p className="typo-normal">
-                            Her kan du legge ut intervju, brukertester og spørreundersøkelser en
-                            bruker kan melde sin interesse i å delta på.
-                        </p>
-                    }
-                />
+                <LenkepanelBase href="#" border>
+                    <Hovedomrade
+                        tittel={'Mine samtykkeskjemaer'}
+                        toppIkon={<FileContent />}
+                        hovedInnhold={
+                            <p className="typo-normal">
+                                Her kan du se, redigere eller slette dine opprettede
+                                samtykkeskjemaer. Du finner også lenken du kan dele med brukeren.
+                            </p>
+                        }
+                    />
+                </LenkepanelBase>
+                <LenkepanelBase href="#" border>
+                    <Hovedomrade
+                        tittel={'Opprett samtykkeskjema'}
+                        toppIkon={<AddCircle />}
+                        hovedInnhold={
+                            <p className="typo-normal">
+                                Her kan du opprette samtykkeskjema til brukertester, intervjuer og
+                                spørreundersøkelser som du kan dele med brukeren.
+                            </p>
+                        }
+                    />
+                </LenkepanelBase>
+                <LenkepanelBase href="#" border>
+                    <Hovedomrade
+                        tittel={'Aktiviteter'}
+                        toppIkon={<Calender />}
+                        hovedInnhold={
+                            <p className="typo-normal">
+                                Her kan du legge ut intervju, brukertester og spørreundersøkelser en
+                                bruker kan melde sin interesse i å delta på.
+                            </p>
+                        }
+                    />
+                </LenkepanelBase>
             </div>
         </div>
     )
