e00cb7701a1b802b694d0ba1b8904df29e53f537
Thomas Rankin <thomas.rankin@nav.no>
2021-08-05 13:26:10 +0200

    Fjernet alle Links og brukt useHistory i stedet

--- frontend/src/containere/knapper/BrukerKnappToppBar.tsx
+++ frontend/src/containere/knapper/BrukerKnappToppBar.tsx
@@ -1,19 +1,15 @@
 import React from 'react'
 import { People } from '@navikt/ds-icons'
 import { Flatknapp } from 'nav-frontend-knapper'
-import { BrowserRouter as Router, Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 export default function BrukerKnappToppBar(): React.ReactElement {
+    const history = useHistory()
+
     return (
-        <Router>
-            <Link to="/samtykkeskjema" style={{ textDecoration: 'none' }}>
-                <Flatknapp className="bruker-knapp">
-                    <span>Ola Nordmann</span>
-                    <People
-                        style={{ height: '1.5rem', width: '1.375rem', marginLeft: '0.75rem' }}
-                    />
-                </Flatknapp>
-            </Link>
-        </Router>
+        <Flatknapp className="bruker-knapp" onClick={() => history.push('/profil')}>
+            <span>Ola Nordmann</span>
+            <People style={{ height: '1.5rem', width: '1.375rem', marginLeft: '0.75rem' }} />
+        </Flatknapp>
     )
 }
--- frontend/src/visninger/adminside/AdminProfilside.tsx
+++ frontend/src/visninger/adminside/AdminProfilside.tsx
@@ -27,7 +27,7 @@ export default function AdminProfilside(): React.ReactElement {
                         }
                     />
                 </LenkepanelBase>
-                <LenkepanelBase href="#" border>
+                <LenkepanelBase href="" border>
                     <Hovedomrade
                         tittel={'Opprett samtykkeskjema'}
                         toppIkon={<AddCircle />}
@@ -39,7 +39,7 @@ export default function AdminProfilside(): React.ReactElement {
                         }
                     />
                 </LenkepanelBase>
-                <LenkepanelBase href="#" border>
+                <LenkepanelBase href="" border>
                     <Hovedomrade
                         tittel={'Aktiviteter'}
                         toppIkon={<Calender />}
