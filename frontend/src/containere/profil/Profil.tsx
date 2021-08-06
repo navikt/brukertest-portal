e7c4ad9148d738df02a2acb5725d83f6feff0b63
Thomas Rankin <thomas.rankin@nav.no>
2021-08-06 09:57:45 +0200

    Flyttet  profilsiden til eget komponent som mottar props

---
+++ frontend/src/containere/profil/Profil.tsx
@@ -0,0 +1,65 @@
+import { Systemtittel } from 'nav-frontend-typografi'
+import React from 'react'
+import Hovedomrade from '../../komponenter/hovedomrade/Hovedomrade'
+import { ReactComponent as ProfilIkon } from '../../style/ressurser/Profil-ikon.svg'
+import { useHistory } from 'react-router-dom'
+
+export interface profilProps {
+    brukernavn: string
+    linkVenstreRoutePath: string
+    linkMidtenRoutePath: string
+    linkHøyreRoutePath?: string
+    linkVenstreIkon: React.ReactElement
+    linkMidtenIkon: React.ReactElement
+    linkHøyreIkon: React.ReactElement
+    linkVenstreTittel: string
+    linkMidtenTittel: string
+    linkHøyreTittel: string
+    linkVenstreInnhold: string
+    linkMidtenInnhold: string
+    linkHøyreInnhold: string
+}
+
+export default function Profil(props: profilProps): React.ReactElement {
+    const history = useHistory()
+
+    // Hvert hovedomrade blir wrappet inni buttons for å sikre universelt design
+
+    return (
+        <div className="profil">
+            <div>
+                <ProfilIkon className="profil-ikon" />
+                <Systemtittel className="brukernavn">{props.brukernavn}</Systemtittel>
+            </div>
+            <div className="meny-items">
+                <button
+                    className="meny-item"
+                    onClick={() => history.push(props.linkVenstreRoutePath)}
+                >
+                    <Hovedomrade
+                        tittel={props.linkVenstreTittel}
+                        toppIkon={props.linkVenstreIkon}
+                        hovedInnhold={<p className="typo-normal">{props.linkVenstreInnhold}</p>}
+                    />
+                </button>
+                <button
+                    className="meny-item"
+                    onClick={() => history.push(props.linkMidtenRoutePath)}
+                >
+                    <Hovedomrade
+                        tittel={props.linkMidtenTittel}
+                        toppIkon={props.linkMidtenIkon}
+                        hovedInnhold={<p className="typo-normal">{props.linkMidtenInnhold}</p>}
+                    />
+                </button>
+                <button className="meny-item">
+                    <Hovedomrade
+                        tittel={props.linkHøyreTittel}
+                        toppIkon={props.linkHøyreIkon}
+                        hovedInnhold={<p className="typo-normal">{props.linkHøyreInnhold}</p>}
+                    />
+                </button>
+            </div>
+        </div>
+    )
+}
--- frontend/src/visninger/admin/AdminProfilside.tsx
+++ frontend/src/visninger/admin/AdminProfilside.tsx
@@ -1,65 +1,25 @@
 import { AddCircle, Calender, FileContent } from '@navikt/ds-icons'
-import { Systemtittel } from 'nav-frontend-typografi'
 import React from 'react'
-import Hovedomrade from '../../komponenter/hovedomrade/Hovedomrade'
-import { ReactComponent as ProfilIkon } from '../../style/ressurser/Profil-ikon.svg'
-import { useHistory } from 'react-router-dom'
+import Profil from '../../containere/profil/Profil'
 
 export default function AdminProfilside(): React.ReactElement {
-    const history = useHistory()
-
-    // Hvert hovedomrade blir wrappet inni buttons for å sikre universelt design
-
     return (
-        <div className="admin-profilside">
-            <div>
-                <ProfilIkon className="profil-ikon" />
-                <Systemtittel className="brukernavn">Admin Istrator</Systemtittel>
-            </div>
-            <div className="meny-items">
-                <button
-                    className="meny-item"
-                    onClick={() => history.push('/admin/mine-samtykkeskjemaer')}
-                >
-                    <Hovedomrade
-                        tittel={'Mine samtykkeskjemaer'}
-                        toppIkon={<FileContent />}
-                        hovedInnhold={
-                            <p className="typo-normal">
-                                Her kan du se, redigere eller slette dine opprettede
-                                samtykkeskjemaer. Du finner også lenken du kan dele med brukeren.
-                            </p>
-                        }
-                    />
-                </button>
-                <button
-                    className="meny-item"
-                    onClick={() => history.push('/admin/opprett-samtykkeskjema')}
-                >
-                    <Hovedomrade
-                        tittel={'Opprett samtykkeskjema'}
-                        toppIkon={<AddCircle />}
-                        hovedInnhold={
-                            <p className="typo-normal">
-                                Her kan du opprette samtykkeskjema til brukertester, intervjuer og
-                                spørreundersøkelser som du kan dele med brukeren.
-                            </p>
-                        }
-                    />
-                </button>
-                <button className="meny-item">
-                    <Hovedomrade
-                        tittel={'Aktiviteter'}
-                        toppIkon={<Calender />}
-                        hovedInnhold={
-                            <p className="typo-normal">
-                                Her kan du legge ut intervju, brukertester og spørreundersøkelser en
-                                bruker kan melde sin interesse i å delta på.
-                            </p>
-                        }
-                    />
-                </button>
-            </div>
-        </div>
+        <Profil
+            brukernavn="Admin Istrator"
+            linkVenstreRoutePath="/admin/mine-samtykkeskjemaer"
+            linkVenstreIkon={<FileContent />}
+            linkVenstreTittel="Mine samtykkeskjemaer"
+            linkVenstreInnhold="Her kan du se, redigere eller slette dine opprettede
+                                samtykkeskjemaer. Du finner også lenken du kan dele med brukeren."
+            linkMidtenRoutePath="/admin/opprett-samtykkeskjema"
+            linkMidtenIkon={<AddCircle />}
+            linkMidtenTittel="Opprett samtykkeskjema"
+            linkMidtenInnhold="Her kan du se, redigere eller slette dine opprettede
+                samtykkeskjemaer. Du finner også lenken du kan dele med brukeren."
+            linkHøyreIkon={<Calender />}
+            linkHøyreTittel="Aktiviteter"
+            linkHøyreInnhold="Her kan du legge ut intervju, brukertester og spørreundersøkelser en
+                bruker kan melde sin interesse i å delta på."
+        />
     )
 }
