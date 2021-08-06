abd516462e82064ae7675622f4dc8cdcf017f788
Thomas Rankin <thomas.rankin@nav.no>
2021-08-06 16:15:28 +0200

    Laget ferdig "mine samtykkeskjemaer" -side

--- frontend/src/visninger/admin/MineSamtykkeskjemaer.tsx
+++ frontend/src/visninger/admin/MineSamtykkeskjemaer.tsx
@@ -1,38 +1,21 @@
-import { Delete, Edit, Eye, FileContent } from '@navikt/ds-icons'
-import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel'
-import { Flatknapp, Hovedknapp } from 'nav-frontend-knapper'
-import { Normaltekst, Systemtittel } from 'nav-frontend-typografi'
+import { FileContent } from '@navikt/ds-icons'
+import { Systemtittel } from 'nav-frontend-typografi'
 import React from 'react'
 import Hovedomrade from '../../komponenter/hovedomrade/Hovedomrade'
-import KopierLenke from '../../komponenter/kopierLenke/KopierLenke'
+import SamtykkeskjemaListePanel from '../../komponenter/samtykkeskjemaListePanel/SamtykkeskjemaListePanel'
 
 export default function OpprettSamtykke(): React.ReactElement {
     const Innhold = () => (
         <>
-            <Systemtittel className="listetittel">{'Aktive:'}</Systemtittel>
-            <Ekspanderbartpanel
-                tittel={<Normaltekst>Klikk her for å åpne/lukke panelet</Normaltekst>}
-                className="ekspanderbart-panel"
-            >
-                <KopierLenke />
-                <div className="skjemaknapper">
-                    <Flatknapp>
-                        <Eye />
-                        <span>Se samtykkeskjema</span>
-                    </Flatknapp>
-                    <Flatknapp>
-                        <Edit />
-                        <span>Rediger</span>
-                    </Flatknapp>
-                    <Flatknapp>
-                        <Delete />
-                        <span>Slett</span>
-                    </Flatknapp>
-                </div>
-            </Ekspanderbartpanel>
+            <Systemtittel className="listetittel">{'Aktive skjemaer:'}</Systemtittel>
+            <SamtykkeskjemaListePanel skjemanavn="Intervju om arbeidsledighet" />
+            <SamtykkeskjemaListePanel skjemanavn="Brukertest av arbeidsplassen" />
+            <Systemtittel className="listetittel" id="listetittel2">
+                {'Tidligere brukte skjemaer:'}
+            </Systemtittel>
+            <SamtykkeskjemaListePanel skjemanavn="Intervju om nav.no" />
         </>
     )
-
     return (
         <div className="mine-samtykkeskjemaer">
             <Hovedomrade
