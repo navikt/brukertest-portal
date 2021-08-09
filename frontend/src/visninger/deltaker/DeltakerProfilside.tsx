3a0eabfd7ef8623481a0dcc3c71985aacf210a2d
Thomas Rankin <thomas.rankin@nav.no>
2021-08-09 13:54:10 +0200

    Flyttet  profilsiden til eget komponent som mottar props

--- frontend/src/visninger/deltaker/DeltakerProfilside.tsx
+++ frontend/src/visninger/deltaker/DeltakerProfilside.tsx
@@ -1,64 +1,25 @@
 import { Caseworker, FileContent, Search } from '@navikt/ds-icons'
 import { Systemtittel } from 'nav-frontend-typografi'
 import React from 'react'
-import Hovedomrade from '../../komponenter/hovedomrade/Hovedomrade'
-import { ReactComponent as ProfilIkon } from '../../style/ressurser/Profil-ikon.svg'
-import { useHistory } from 'react-router-dom'
+import Profil from '../../containere/profil/Profil'
 
 export default function DeltakerProfilside(): React.ReactElement {
-    const history = useHistory()
-
-    // Hvert hovedomrade blir wrappet inni buttons for å sikre universelt design
-
     return (
-        <div className="deltaker-profilside">
-            <div>
-                <ProfilIkon className="profil-ikon" />
-                <Systemtittel className="brukernavn">Ola Nordmann</Systemtittel>
-            </div>
-            <div className="meny-items">
-                <button
-                    className="meny-item"
-                    onClick={() => history.push('/admin/mine-samtykkeskjemaer')}
-                >
-                    <Hovedomrade
-                        tittel={'Mine samtykker'}
-                        toppIkon={<FileContent />}
-                        hovedInnhold={
-                            <p className="typo-normal">
-                                Her kan du se, redigere eller slette de samtykkeskjemaene du har
-                                singert.
-                            </p>
-                        }
-                    />
-                </button>
-                <button
-                    className="meny-item"
-                    onClick={() => history.push('/admin/opprett-samtykkeskjema')}
-                >
-                    <Hovedomrade
-                        tittel={'Hva kan jeg bli med på?'}
-                        toppIkon={<Search />}
-                        hovedInnhold={
-                            <p className="typo-normal">
-                                En oversikt over framtidige brukertester, intervjuer og
-                                spørreundersøkelser du kan melde din interesse til.
-                            </p>
-                        }
-                    />
-                </button>
-                <button className="meny-item">
-                    <Hovedomrade
-                        tittel={'Mine kontaktopplysninger'}
-                        toppIkon={<Caseworker />}
-                        hovedInnhold={
-                            <p className="typo-normal">
-                                Her kan du se hvilken informasjon du har registrert.
-                            </p>
-                        }
-                    />
-                </button>
-            </div>
-        </div>
+        <Profil
+            brukernavn="Ola Nordmann"
+            linkVenstreRoutePath="/admin/mine-samtykkeskjemaer"
+            linkVenstreIkon={<FileContent />}
+            linkVenstreTittel="Mine samtykkeskjemaer"
+            linkVenstreInnhold="Her kan du se, redigere eller slette de samtykkeskjemaene du har
+                singert."
+            linkMidtenRoutePath="/admin/opprett-samtykkeskjema"
+            linkMidtenIkon={<Search />}
+            linkMidtenTittel="Hva kan jeg bli med på?"
+            linkMidtenInnhold="En oversikt over framtidige brukertester, intervjuer og
+                spørreundersøkelser du kan melde din interesse til."
+            linkHøyreIkon={<Caseworker />}
+            linkHøyreTittel="Mine kontaktopplysninger"
+            linkHøyreInnhold="Her kan du se hvilken informasjon du har registrert."
+        />
     )
 }
