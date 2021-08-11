c46dfa28d245b042a01daebeeabafb7b46b25b1f
Thomas Rankin <thomas.rankin@nav.no>
2021-08-11 12:56:14 +0200

    Noen småendringer

--- frontend/src/containere/samtykkeskjema/SamtykkeStegindikator.tsx
+++ frontend/src/containere/samtykkeskjema/SamtykkeStegindikator.tsx
@@ -9,8 +9,11 @@ export default function SamtykkeStegindikator(): React.ReactElement {
             <Stegindikator
                 steg={[
                     { label: 'Hva skal undersøkelsen brukes til?', index: 0 },
-                    { label: 'Hvem er vi og hvilke metoder bruker vi?', index: 1 },
-                    { label: 'Om personopplysninger', index: 2 },
+                    {
+                        label: 'Hvem er ansvarlig og hvordan blir opplysninger samlet inn?',
+                        index: 1,
+                    },
+                    { label: 'Hvordan behandles opplysninger?', index: 2 },
                     { label: 'Om dine rettigheter', index: 3 },
                     { label: 'Jeg forstår - erklæring', index: 4 },
                     { label: 'Utfyllingssteg', index: 5 },
